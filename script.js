d120c569c6e48d3830a4548383f717ce9e62a16f
Michael Argent <michael.r.argent@gmail.com>
2019-11-25 09:23:09 -0600

    final

--- script.js
+++ script.js
@@ -2,81 +2,81 @@
 
 // MM-DD
 function getAstroPicOfDay(monthInput, dayInput) {
-    monthInput = makeDouble(monthInput);
-    dayInput = makeDouble(dayInput);
-    console.log("expecting doubles", monthInput, dayInput);
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=JFlugkHkjMcUZqba55Q0oavmdbGqZBzHHAEPan3A&date=2005-${monthInput}-${dayInput}`)
-        .then(response => response.json())
-        .then(responseJson =>
-            displayResultsAstro(responseJson))
-        .catch(error => {
-            console.log(error)
-        });
+    monthInput = makeDouble(monthInput); 
+    dayInput = makeDouble(dayInput); 
+    console.log("expecting doubles", monthInput, dayInput); 
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=JFlugkHkjMcUZqba55Q0oavmdbGqZBzHHAEPan3A&date=2018-${monthInput}-${dayInput}`)
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResultsAstro(responseJson))
+      .catch(error => {
+        console.log(error)
+      });
 }
 
 // M-D
 function getWikiDay(monthInput, dayInput) {
-    monthInput = stripLeadingZeros(monthInput);
-    dayInput = stripLeadingZeros(dayInput);
-    console.log("expecting singles", monthInput, dayInput);
+  monthInput = stripLeadingZeros(monthInput); 
+    dayInput = stripLeadingZeros(dayInput); 
+  console.log("expecting singles" , monthInput, dayInput);
     fetch(`https://byabbe.se/on-this-day/${monthInput}/${dayInput}/events.json`)
-        .then(response => response.json())
-        .then(responseJson =>
-            displayResultsWikiDay(responseJson))
-        .catch(error => {
-            console.log(error)
-        });
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResultsWikiDay(responseJson))
+      .catch(error => {
+        console.log(error)
+      });
+  }
+
+
+function makeDouble(date){
+  date = date.length == 1 ? "0" + date : date;
+  return date;
 }
 
-
-function makeDouble(date) {
-    date = date.length == 1 ? "0" + date : date;
-    return date;
-}
-
-function stripLeadingZeros(date) {
-    date = date[0] === '0' ? date[1] : date;
-    return date;
+function stripLeadingZeros(date){
+  date = date[0] === '0' ? date[1] : date; 
+  return date; 
 }
 
 
 function displayResultsWikiDay(responseJson) {
-    // console.log(responseJson);
-    let html = `<h1>This happened on that day:</h1>`;
-    for (let i = 0; i < 10; i++) {
-        html += `<div class = "resultsWikiDay">`
-        let evt = responseJson.events[i];
-        html += `<div class="year">${evt.year}</div>
-    <div class="description">${evt.description}</div>`;
-        evt.wikipedia.forEach(function (link) {
-            console.log(link);
-            html += `<div><p>For more information on ${link.title}, click here: <a href="${link.wikipedia}">${link.title}</a></p></div>`;
-        });
-        html += `</div>`
-    }
-    $(".results").html(html);
+ // console.log(responseJson);
+  let html = `<h1>This happened on that day:</h1>` ; 
+  for(let i=0; i < 10; i++){
+    html += `<div class = "resultsWikiDay">`
+    let evt = responseJson.events[i]; 
+    html += `<div class="year">${evt.year}</div>
+    <div class="description">${evt.description}</div>`; 
+    evt.wikipedia.forEach(function(link){
+      console.log(link); 
+      html += `<div><p>For more information on ${link.title}, click here: <a href="${link.wikipedia}">${link.title}</a></p></div>`; 
+    }); 
+  html += `</div>`
+} 
+  $(".results").html(html); 
 }
 
 
-function displayResultsAstro(responseJson) {
-    console.log(responseJson);
-    $(".results").prepend(`<h1>The cosomos looked like this on that day:</h1> 
+function displayResultsAstro(responseJson){
+  console.log(responseJson);
+  $(".results").prepend(`<h1>The cosomos looked like this on that day:</h1> 
   <img src="${responseJson.url}" class="astroResults">`);
 }
 
 
 function watchSubmit() {
-    $('form').submit(event => {
-        event.preventDefault();
-        let monthInput = $(event.target).find('#monthInput').val()
-        let dayInput = $(event.target).find('#dayInput').val()
-        getWikiDay(monthInput, dayInput);
-        getAstroPicOfDay(monthInput, dayInput);
-    });
+  $('form').submit(event => {
+    event.preventDefault();
+    let monthInput = $(event.target).find('#monthInput').val()
+    let dayInput = $(event.target).find('#dayInput').val()
+    getWikiDay(monthInput, dayInput);
+    getAstroPicOfDay(monthInput, dayInput);
+  });
 }
 
 
-$(function () {
-    console.log('App loaded! Waiting for submit!');
-    watchSubmit();
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchSubmit();
 });
